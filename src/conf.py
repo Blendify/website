737dc86cfc92ff609af52411de990f23dfaf1873
Aaron Carlisle <carlisle.b3d@gmail.com>
2019-08-05 20:03:25 -0400

    Use new theme

--- src/conf.py
+++ src/conf.py
@@ -91,7 +91,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "basic"
+html_theme = "theme"
+html_theme_path = ["../"]
 
 html_copy_source = False
 html_show_sphinx = False
